be271cfae8691cb1d58d8112cc79c889958f1a31
yhorie <yuk.horie@gmail.com>
2022-09-24 14:45:38 +0900

    Add multi-modle-editor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,49 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Editor from '@monaco-editor/react'
+import { useState, useRef, useEffect } from 'react'
+
+import files from './data/files'
 
 const Home: NextPage = () => {
-  return <div></div>
+  const editorRef = useRef(null)
+  const [fileName, setFileName] = useState('script.js')
+
+  const file = files[fileName]
+
+  useEffect(() => {
+    editorRef.current?.focus()
+  }, [file.name])
+
+  return (
+    <div>
+      <button
+        disabled={fileName === 'script.js'}
+        onClick={() => setFileName('script.js')}
+      >
+        script.js
+      </button>
+      <button
+        disabled={fileName === 'style.css'}
+        onClick={() => setFileName('style.css')}
+      >
+        style.css
+      </button>
+      <button
+        disabled={fileName === 'index.html'}
+        onClick={() => setFileName('index.html')}
+      >
+        index.html
+      </button>
+      <Editor
+        height="80vh"
+        theme="vs-dark"
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+        onMount={(editor) => (editorRef.current = editor)}
+      />
+    </div>
+  )
 }
 
 export default Home
