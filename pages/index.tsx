a9e0f78a137c918e0a3b330703dcf88328a8b649
yhorie <yuk.horie@gmail.com>
2022-09-24 22:43:10 +0900

    Add change lang id

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,57 @@
 import type { NextPage } from 'next'
-import Editor from '@monaco-editor/react'
+import Editor, { Monaco, useMonaco } from '@monaco-editor/react'
 import { useState, useRef, useEffect } from 'react'
 import type { editor } from 'monaco-editor'
 
 import files from './data/files'
 
 const Home: NextPage = () => {
-  const editorRef = useRef(null)
+  const editorRef = useRef<editor.IStandaloneCodeEditor>(null)
+  const monacoRef = useRef<Monaco>(null)
   const [fileName, setFileName] = useState('script.js')
   const [lang, setLang] = useState('')
+  const [langs, setLangs] = useState([])
 
   const file = files[fileName]
-
   useEffect(() => {
     editorRef.current?.focus()
   }, [file.name])
 
-  const onMountHandler = (editor: editor.IStandaloneCodeEditor) => {
+  const onMountHandler = (
+    editor: editor.IStandaloneCodeEditor,
+    monaco: Monaco
+  ) => {
     setLang(editor.getModel()?.getLanguageId())
+    setLangs(monaco.languages.getLanguages())
     editor.onDidChangeModel(({ newModelUrl }) => {
       setLang(editor.getModel()?.getLanguageId())
     })
     editorRef.current = editor
+    monacoRef.current = monaco
+  }
+  const renderLangsList = () => {
+    return (
+      <select
+        value={lang}
+        onChange={(e) => {
+          const langId = e.target.value
+          setLang(langId)
+          monacoRef.current?.editor.setModelLanguage(
+            editorRef.current?.getModel(),
+            langId
+          )
+        }}
+      >
+        {langs.map((e) => (
+          <option key={e.id}>{e.id}</option>
+        ))}
+      </select>
+    )
   }
 
   return (
     <div>
+      <div>{renderLangsList()}</div>
       <button
         disabled={fileName === 'script.js'}
         onClick={() => setFileName('script.js')}
