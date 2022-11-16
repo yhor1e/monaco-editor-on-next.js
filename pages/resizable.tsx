509cb68a4f334762da29ae5025f7f583c88d5af7
yhorie <yuk.horie@gmail.com>
2022-11-16 11:55:10 +0900

    In the middle of createing resizable layout page

--- pages/resizable.tsx
+++ pages/resizable.tsx
@@ -1,61 +1,141 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const ResizablePage: NextPage = () => {
-  const [horizontalFlexPositon, sethorizontalFlexPositon] = useState<number[]>([
-    33, 34, 67, 68, 100,
-  ])
+  const initFraction = [20, 40, 40]
+  const [colFraction, setColFraction] = useState<number[]>([])
+  const refContainer = useRef<HTMLDivElement | null>(null)
+  const [isDragging, setIsDragging] = useState<Boolean>(false)
+
+  const resizeHandler = () => {
+    console.log('resize')
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeHandler)
+    return () => {
+      return window.removeEventListener('resize', resizeHandler)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isDragging) {
+    } else {
+    }
+  }, [isDragging])
+
+  useEffect(() => {
+    if (!refContainer.current) return
+    const width = Number(
+      window.getComputedStyle(refContainer.current).width.replace('px', '')
+    )
+    const elmentsWidth = initFraction.map((w) => (width * w) / 100)
+    const modified = elmentsWidth.map((width, index) => {
+      return elmentsWidth.slice(0, index + 1).reduce((acc, value) => {
+        return acc + value
+      }, 0)
+    })
+    setColFraction(modified)
+  }, [refContainer.current])
+
+  const onDragHandler = (e) => {
+    //if (!isDragging) return
+    if (e.clientX === 0) return
+    const pos = Number(e.target.getAttribute('data-diviver-pos'))
+    setColFraction((prev) => {
+      const modified = [0, ...prev]
+      if (modified[pos - 1] < e.clientX && modified[pos + 1] > e.clientX) {
+        modified[pos] = e.clientX
+      }
+      modified.shift()
+      return modified
+    })
+  }
+
+  const onMouseDown = (e) => {
+    console.log('onMouseDown')
+    document.addEventListener('mouseup', onMouseUp)
+    document.addEventListener('mousemove', onDragHandler)
+    //setIsDragging(true)
+  }
+
+  const onMouseUp = (e) => {
+    console.log('onMouseUp')
+    document.removeEventListener('mouseup', onMouseUp)
+    document.removeEventListener('mousemove', onDragHandler)
+    //setIsDragging(false)
+  }
 
   return (
-    <div className="columns">
-      <div className="column" style={{ flex: `${horizontalFlexPositon[0]}%` }}>
-        Column 1: Put any relevant content here
+    <div
+      ref={refContainer}
+      className="container"
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <div>
+        <div
+          onMouseDown={onMouseDown}
+          data-diviver-pos="1"
+          className="diviver"
+          style={{
+            position: 'absolute',
+            backgroundColor: 'black',
+            padding: '0 4px',
+            left: `${colFraction[0]}px`,
+            height: '100%',
+            zIndex: '2',
+            transform: 'translate(-50%, 0)',
+          }}
+        ></div>
+        <div
+          onMouseDown={onMouseDown}
+          onDragStart={(event) => {
+            event.dataTransfer.clearData()
+          }}
+          onDragOver={(e) => {
+            e.preventDefault()
+          }}
+          data-diviver-pos="2"
+          className="diviver"
+          style={{
+            position: 'absolute',
+            left: `${colFraction[1]}px`,
+            height: '100%',
+          }}
+        ></div>
       </div>
       <div
         style={{
-          flex: `${horizontalFlexPositon[1] - horizontalFlexPositon[0]}%`,
-        }}
-        onDrag={(e) => {
-          console.log('onDrag')
-          console.log(e.screenX)
-          console.log(window.innerWidth)
-          console.log((e.screenX / window.innerWidth) * 100)
-          sethorizontalFlexPositon((prev) => {
-            const current = [...prev]
-            if (
-              (e.clientX / window.innerWidth) * 100 > 10 &&
-              (e.clientX / window.innerWidth) * 100 < current[2] - 10
-            ) {
-              current[0] = (e.clientX / window.innerWidth) * 100 - 1
-              current[1] = (e.clientX / window.innerWidth) * 100
-              console.log(current)
-            }
-            return current
-          })
-        }}
-        className="diviver"
-      ></div>
-      <div
-        className="column"
-        style={{
-          flex: `${horizontalFlexPositon[2] - horizontalFlexPositon[1]}%`,
-        }}
-      >
-        Column 2: Put any relevant content here{' '}
-      </div>
-      <div
-        className="diviver"
-        style={{
-          flex: `${horizontalFlexPositon[3] - horizontalFlexPositon[2]}%`,
+          position: 'absolute',
+          left: 0,
+          width: `${colFraction[0]}px`,
+          height: '100%',
+          backgroundColor: 'pink',
         }}
       ></div>
       <div
-        className="column"
         style={{
-          flex: `${horizontalFlexPositon[4] - horizontalFlexPositon[3]}%`,
+          position: 'absolute',
+          left: `${colFraction[0]}px`,
+          width: `${colFraction[1] - colFraction[0]}px`,
+          height: '100%',
+          backgroundColor: 'skyblue',
+        }}
+      ></div>
+      <div
+        style={{
+          position: 'absolute',
+          left: `${colFraction[1]}px`,
+          width: `${colFraction[2] - colFraction[1]}px`,
+          height: '100%',
+          backgroundColor: 'grey',
         }}
       >
-        Column 3: Put any relevant content here
+        right
       </div>
     </div>
   )
