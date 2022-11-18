0eda575229a87d96da9b79178fc3d159ce893905
yhorie <yuk.horie@gmail.com>
2022-11-19 01:00:34 +0900

    Add resize feature

--- pages/resizable.tsx
+++ pages/resizable.tsx
@@ -1,30 +1,14 @@
 import type { NextPage } from 'next'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 
 const ResizablePage: NextPage = () => {
-  const initFraction = [20, 40, 40]
   const [colFraction, setColFraction] = useState<number[]>([])
-  const refContainer = useRef<HTMLDivElement | null>(null)
   const [isDragging, setIsDragging] = useState<Boolean>(false)
+  const refContainer = useRef<HTMLDivElement | null>(null)
+  const refDrag = useRef<number | null>(null)
 
-  const resizeHandler = () => {
-    console.log('resize')
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', resizeHandler)
-    return () => {
-      return window.removeEventListener('resize', resizeHandler)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (isDragging) {
-    } else {
-    }
-  }, [isDragging])
-
-  useEffect(() => {
+  const setColFractionFromInit = useCallback(() => {
+    const initFraction = [20, 40, 40]
     if (!refContainer.current) return
     const width = Number(
       window.getComputedStyle(refContainer.current).width.replace('px', '')
@@ -36,14 +20,42 @@ const ResizablePage: NextPage = () => {
       }, 0)
     })
     setColFraction(modified)
-  }, [refContainer.current])
+  }, [])
 
-  const onDragHandler = (e) => {
-    //if (!isDragging) return
+  useEffect(() => {
+    const resizeHandler = () => {
+      setColFractionFromInit()
+    }
+    window.addEventListener('resize', resizeHandler)
+    return () => {
+      return window.removeEventListener('resize', resizeHandler)
+    }
+  }, [setColFractionFromInit])
+
+  useEffect(() => {
+    if (isDragging === false) return
+    window.addEventListener('mouseup', onMouseUp)
+    window.addEventListener('mousemove', onMoveHandler)
+    console.log(isDragging)
+    return () => {
+      window.removeEventListener('mouseup', onMouseUp)
+      window.removeEventListener('mousemove', onMoveHandler)
+      console.log(isDragging)
+    }
+  }, [isDragging])
+
+  useEffect(() => {
+    setColFractionFromInit()
+  }, [setColFractionFromInit])
+
+  const onMoveHandler = (e) => {
+    if (!isDragging) return
     if (e.clientX === 0) return
-    const pos = Number(e.target.getAttribute('data-diviver-pos'))
+
     setColFraction((prev) => {
+      if (!refDrag.current) return prev
       const modified = [0, ...prev]
+      const pos = refDrag.current
       if (modified[pos - 1] < e.clientX && modified[pos + 1] > e.clientX) {
         modified[pos] = e.clientX
       }
@@ -54,16 +66,14 @@ const ResizablePage: NextPage = () => {
 
   const onMouseDown = (e) => {
     console.log('onMouseDown')
-    document.addEventListener('mouseup', onMouseUp)
-    document.addEventListener('mousemove', onDragHandler)
-    //setIsDragging(true)
+    refDrag.current = Number(e.target.getAttribute('data-diviver-pos'))
+    setIsDragging(true)
   }
 
   const onMouseUp = (e) => {
     console.log('onMouseUp')
-    document.removeEventListener('mouseup', onMouseUp)
-    document.removeEventListener('mousemove', onDragHandler)
-    //setIsDragging(false)
+    refDrag.current = null
+    setIsDragging(false)
   }
 
   return (
@@ -82,29 +92,15 @@ const ResizablePage: NextPage = () => {
           data-diviver-pos="1"
           className="diviver"
           style={{
-            position: 'absolute',
-            backgroundColor: 'black',
-            padding: '0 4px',
             left: `${colFraction[0]}px`,
-            height: '100%',
-            zIndex: '2',
-            transform: 'translate(-50%, 0)',
           }}
         ></div>
         <div
           onMouseDown={onMouseDown}
-          onDragStart={(event) => {
-            event.dataTransfer.clearData()
-          }}
-          onDragOver={(e) => {
-            e.preventDefault()
-          }}
           data-diviver-pos="2"
           className="diviver"
           style={{
-            position: 'absolute',
             left: `${colFraction[1]}px`,
-            height: '100%',
           }}
         ></div>
       </div>
@@ -116,7 +112,9 @@ const ResizablePage: NextPage = () => {
           height: '100%',
           backgroundColor: 'pink',
         }}
-      ></div>
+      >
+        left
+      </div>
       <div
         style={{
           position: 'absolute',
@@ -125,7 +123,9 @@ const ResizablePage: NextPage = () => {
           height: '100%',
           backgroundColor: 'skyblue',
         }}
-      ></div>
+      >
+        center
+      </div>
       <div
         style={{
           position: 'absolute',
